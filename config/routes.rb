9a3086dd57a42871439af756c05bd146a6fc5a69
ryota-akamatsu <ra.ra.3.cp.3@gmail.com>
2021-01-19 20:21:35 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to:  "messages#index"
-resources :users,only:[:edit,:update,:destroy,]
-resources :rooms,only:[:new,:create]
+root to: "rooms#index"
+resources :users, only: [:edit, :update]
+resources :rooms, only: [:new, :create]
 end
